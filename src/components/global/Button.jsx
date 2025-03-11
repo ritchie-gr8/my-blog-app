e37f84fce8818ce617f4b3c9c792c7793a303b95
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-11 17:16:36 +0700

    feat(navbar): add click event on hamburger to show menu on mobile sceen

--- src/components/global/Button.jsx
+++ src/components/global/Button.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { BUTTONS_VARIANT } from '../../constants/buttonVariants'
+import React from "react";
+import { BUTTONS_VARIANT } from "../../constants/buttonVariants";
 
-const Button = ({children, variant}) => {
+const Button = ({ children, variant, handleOnClick }) => {
   return (
-    <button className={`px-10 py-3 cursor-pointer rounded-full ${BUTTONS_VARIANT[variant] ?? BUTTONS_VARIANT.primary}`}>
-        {children}
+    <button
+      onClick={handleOnClick}
+      className={`px-10 py-3 cursor-pointer rounded-full ${
+        BUTTONS_VARIANT[variant] ?? BUTTONS_VARIANT.primary
+      }`}
+    >
+      {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
