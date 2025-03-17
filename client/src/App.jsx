b9450ee4b91fb794444dfb3806dedc8b3491b8ca
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-17 22:23:03 +0700

    feat: add go backend (health check route) and for frontend fix article card img / test connect to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import Article from "./components/Article";
 import ArticleSection from "./components/ArticleSection";
 import Hero from "./components/Hero";
 import Footer from "./components/global/Footer";
 import Navbar from "./components/global/Navbar";
+import { checkHealth } from "./api/user";
 
 function App() {
+  useEffect(() => {
+    const fetchHealth = async () => {
+      const res = await checkHealth();
+      console.log(res);
+    };
+
+    fetchHealth()
+  }, []);
+
   return (
     <>
       <Navbar />
---
+++ server/internal/env/env.go
@@ -0,0 +1,29 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key, fallback string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+
+	valAsInt, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+
+	return valAsInt
+}
