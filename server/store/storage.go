1846c1f056cd60afc5ccbb35d1697788877b88a6
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-20 01:10:12 +0700

    feat(server): add delete/update endpoints and add comments schema

--- server/store/storage.go
+++ server/store/storage.go
@@ -14,16 +14,23 @@ type Storage struct {
 	Posts interface {
 		GetByID(context.Context, int64) (*Post, error)
 		Create(context.Context, *Post) error
+		Update(context.Context, *Post) error
+		Delete(context.Context, int64) error
 	}
 
 	Users interface {
 		Create(context.Context, *User) error
 	}
+
+	Comments interface {
+		GetByPostID(context.Context, int64) ([]Comment, error)
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostStore{db},
-		Users: &UserStore{db},
+		Posts:    &PostStore{db},
+		Users:    &UserStore{db},
+		Comments: &CommentStore{db},
 	}
 }
