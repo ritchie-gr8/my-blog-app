e7ed49db2dcef37d6dc54d4f1e8c3e031e00f7ef
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-03-20 01:10:12 +0700

    feat(server): add delete/update endpoints and add comments schema

--- server/store/posts.go
+++ server/store/posts.go
@@ -7,15 +7,16 @@ import (
 )
 
 type Post struct {
-	ID             int64  `json:"id"`
-	Title          string `json:"title"`
-	Introduction   string `json:"introduction"`
-	Content        string `json:"content"`
-	Category       string `json:"category"`
-	UserID         int64  `json:"user_id"`
-	ThumbnailImage []byte `json:"thumbnail_image"`
-	CreatedAt      string `json:"created_at"`
-	UpdatedAt      string `json:"updated_at"`
+	ID             int64     `json:"id"`
+	Title          string    `json:"title"`
+	Introduction   string    `json:"introduction"`
+	Content        string    `json:"content"`
+	Category       string    `json:"category"`
+	UserID         int64     `json:"user_id"`
+	ThumbnailImage []byte    `json:"thumbnail_image"`
+	CreatedAt      string    `json:"created_at"`
+	UpdatedAt      string    `json:"updated_at"`
+	Comments       []Comment `json:"comments"`
 }
 
 type PostStore struct {
@@ -78,4 +79,41 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	}
 
 	return &post, nil
+}
+
+func (s *PostStore) Update(ctx context.Context, post *Post) error {
+	query := `
+		UPDATE posts
+		SET title = $1, introduction = $2, content = $3, category = $4, thumbnail_image = $5,
+		updated_at = NOW()
+		WHERE id = $6
+	`
+
+	_, err := s.db.ExecContext(ctx, query, post.Title, post.Introduction, post.Content, post.Category, post.ThumbnailImage, post.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *PostStore) Delete(ctx context.Context, id int64) error {
+	query := `
+		DELETE FROM posts WHERE id = $1
+	`
+	res, err := s.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return ErrNotFound
+	}
+
+	return nil
 }
