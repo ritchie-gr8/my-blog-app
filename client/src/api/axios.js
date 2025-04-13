8a200b8f853971f0640d0e99c9bceb25317453ba
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-13 10:06:59 +0700

    fix: auth flow when signup and login

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -14,18 +14,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-api.interceptors.response.use(
-  response => response,
-  error => {
-    const isLoginRequest = error.config.url.includes('/authentication/token');
-
-    if (error.response && error.response.status === 401 && !isLoginRequest) {
-      localStorage.removeItem("userData");
-      localStorage.removeItem("token");
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
-export default api; 
+export default api;
