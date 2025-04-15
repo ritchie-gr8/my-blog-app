7ac6a1d3bff2d9840e6f1db6b487f76773f9bc2b
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-15 20:55:26 +0700

    feat: add skeleton to landing page

--- client/src/components/PostsSection.jsx
+++ client/src/components/PostsSection.jsx
@@ -8,6 +8,8 @@ import { getCategories } from "@/api/categories";
 import { getFormatedDate } from "@/lib/utils";
 import TablePagination from "@/components/custom/TablePagination";
 import { useDebounce } from "@/hooks/useDebouce";
+import { Skeleton } from "./ui/skeleton";
+import { SearchX } from "lucide-react";
 
 const LIMIT = 6;
 
@@ -17,7 +19,7 @@ const PostsSection = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedSearch = useDebounce(searchTerm, 500);
 
@@ -115,35 +117,61 @@ const PostsSection = () => {
         onSearchChange={handleSearchChange}
         searchTerm={searchTerm}
       />
+      {!isLoading && (!posts || posts?.length === 0) && (
+        <div className="flex flex-col items-center justify-center h-80 mb-12 text-gray-500">
+          <SearchX className="size-12 mb-6" />
+          <p className="text-2xl">No posts found</p>
+        </div>
+      )}
       <article className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3">
-        {posts?.map(
-          ({
-            id,
-            thumbnail_image,
-            category,
-            title,
-            introduction,
-            author_name,
-            author_profile_picture,
-            updated_at,
-          }) => (
-            <Link to={`/posts/${id}`} key={id}>
-              <ArticleCard
-                image={
-                  thumbnail_image.trim() === ""
-                    ? `https://picsum.photos/360x360`
-                    : thumbnail_image
-                }
-                category={category}
-                title={title}
-                description={introduction}
-                author_name={author_name}
-                author_profile_picture={author_profile_picture}
-                date={getFormatedDate(updated_at)}
-              />
-            </Link>
-          )
+        {isLoading && (
+          <>
+            {[...Array(3)].map((_, idx) => (
+              <div
+                key={idx}
+                className="flex flex-col justify-between h-full p-4"
+              >
+                <Skeleton className="w-full h-60 bg-brown-300" />
+                <Skeleton className="w-16 h-6 bg-brown-300 mt-4" />
+                <Skeleton className="w-full h-6 bg-brown-300 mt-4" />
+                <Skeleton className="w-full h-3 bg-brown-300 mt-4" />
+                <div className="flex items-center mt-6">
+                  <Skeleton className="w-6 h-6 bg-brown-300 rounded-full" />
+                  <Skeleton className="w-24 h-3 bg-brown-300 mx-4" />
+                </div>
+              </div>
+            ))}
+          </>
         )}
+        {!isLoading &&
+          posts?.map(
+            ({
+              id,
+              thumbnail_image,
+              category,
+              title,
+              introduction,
+              author_name,
+              author_profile_picture,
+              updated_at,
+            }) => (
+              <Link to={`/posts/${id}`} key={id}>
+                <ArticleCard
+                  image={
+                    thumbnail_image.trim() === ""
+                      ? `https://picsum.photos/360x360`
+                      : thumbnail_image
+                  }
+                  category={category}
+                  title={title}
+                  description={introduction}
+                  author_name={author_name}
+                  author_profile_picture={author_profile_picture}
+                  date={getFormatedDate(updated_at)}
+                />
+              </Link>
+            )
+          )}
       </article>
 
       {totalPages > 1 && (
